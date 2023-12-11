8d9de75dc57ecd3eb88784acd0fca766260bebe9
rabbitbooster <cyber.null.rabbit@gmail.com>
2023-12-11 21:16:18 +0900

    Init Commit

---
+++ Assets/scripts/TargetCode.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class TargetCode
+{
+    public void TestFunc(bool test1, bool test2)
+    {
+        if (test1)
+        {
+            Debug.Log("test1");
+        }
+        else if (test2)
+        {
+            Debug.Log("test2");
+        }
+        else
+        {
+            Debug.Log("else");
+        }
+    }
+}
